cc464e1937919395d4686e300eb6639bae01c4f6
driosoft-pro <deyton007@gmail.com>
2026-03-16 11:38:42 -0500

    fix comparendos

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "postgres",
+    logging: false,
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
+
+export default sequelize;

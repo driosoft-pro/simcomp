0b245126487e94e4b600d533d2144e063b4bfbcc
driosoft-pro <deyton007@gmail.com>
2026-03-18 12:34:10 -0500

    Fixed personas, comparendos

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ import { swaggerUi, swaggerSpec } from "./swagger/swagger.js";
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({
+  hsts: false,
+  contentSecurityPolicy: false,
+}));
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
@@ -21,7 +24,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-app.use("/api/automotores", automotoresRoutes);
+app.use("/api", automotoresRoutes);
 
 /* Swagger documentation */
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
